e8f76e8da37fe3e96cbc3b513079eb92e61e4d4a
Nguemo1123 <99772417+Nguemo1123@users.noreply.github.com>
2022-04-11 22:17:13 -0400

    Corrected API call

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
+let apiKey = "4779942cd0fbaf73cfba882126b42e77";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
